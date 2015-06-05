c8e5c32c9e6ac5aba38318ef17430d4794ee9d08
Morgan Wilde <wilde.morgan@gmail.com>
2015-06-05 18:10:39 +0300

    Question list v1

--- logic.js
+++ logic.js
@@ -1,86 +1,105 @@
-var WelcomeDisplay = React.createClass({
+var SectionHeader = React.createClass({
     render: function() {
-        return React.createElement("div", null, "Welcome!");
-    },
-    bobby: function() {
-        return "bobby";
-    }
-});
-
-var WelcomeWagon = React.createClass({
-    render: function() {
-        return React.createElement("div", null, 
-            React.createElement(WelcomeDisplay, null),
-            React.createElement(WelcomeDisplay, null),
-            React.createElement(WelcomeDisplay, null)
-        );
-    }
-});
-
-var QuestionRow = React.createClass({
-    render: function() {
-        var questionName = typeof this.props.questionName === 'undefined' ? '' : this.props.questionName;
         return (
-            React.createElement('tr', null, 
-                React.createElement('td', null, questionName)
-            )
+            React.createElement('h1', {className: 'page-header'}, this.props.text)
         );
     }
 });
 
 var QuestionsTable = React.createClass({
     getInitialState: function() {
-        return {questionNames: []};
+        return {
+            columns: ['topic', 'board', 'module', 'name', 'parts'],
+            questions: [
+                // {board: 'a1', module: 'a2', name: 'a3', parts: 'a4', topic: 'a5'},
+                // {board: 'b1', module: 'b2', name: 'b3', parts: 'b4', topic: 'b5'}
+            ]
+        };
     },
-    componentDidMount: function() {
-      this.firebaseRef = new Firebase('https://flickering-torch-5178.firebaseio.com/comments');
-      this.firebaseRef.on('value', function(childSnapshot, previousChildName) {
-        var questionNames = this.state.questionNames;
-        questionNames.push(childSnapshot.val())
-        this.setState({questionNames: questionNames});
-      }.bind(this));
+
+    componentWillMount: function() {
+        this.firebaseRef = new Firebase('https://markit-demo.firebaseio.com/20150605-backup/data/questions');
+        this.firebaseRef.on('value', function(snapshot) {
+            var questions = [];
+            snapshot.forEach(function(questionSnapshot) {
+                var questionValue = questionSnapshot.val();
+                var board = typeof questionValue.board === 'undefined' ? questionValue.boards.length : 1;
+
+                questions.push({
+                    board: board,
+                    module: questionValue.module,
+                    name: questionValue.name,
+                    parts: questionValue.parts.length,
+                    topic: questionValue.topic
+                });
+            }.bind(this));
+            this.setState({questions: questions});
+        }.bind(this));
     },
-    componentWillUnmount: function() {
-      this.firebaseRef.off();
+
+    componentWillUnount: function() {
+        this.firebaseRef.off();
     },
+
     render: function() {
+        var columns = this.state.columns;
+        var questions = this.state.questions;
+
+        // Headers
+        var columnHeaders = [];
+        for (var column in columns) {
+            var columnName = columns[column];
+            columnHeaders.push(React.createElement('th', {key: 'header-cell-'+columnName}, columnName));
+        }
+
+        // Data
         var questionRows = [];
-        this.state.questionNames.forEach(function(questionName) {
-            console.log(questionName);
-            questionRows.push(React.createElement(QuestionRow, {questionName: questionName.val()}));
-        });
+        for (var question in questions) {
+            var questionData = questions[question];
+            var questionCells = [];
+            for (column in columns) {
+                var questionCell = React.createElement('td', {key: 'question-cell-'+column}, questionData[columns[column]]);
+                questionCells.push(questionCell);
+            }
+            var questionRow = React.createElement('tr', {key: 'question-row-'+question},
+                questionCells
+            )
+            questionRows.push(questionRow);
+        }
+
         return (
-            React.createElement('table', {className: 'questions-table'}, questionRows)
+            React.createElement('div', {className: 'table-responsive'},
+                React.createElement('table', {className: 'table table-striped'}, 
+                    React.createElement('thead', null, 
+                        React.createElement('tr', null, 
+                            columnHeaders
+                        )
+                    ),
+                    React.createElement('tbody', null,
+                        questionRows
+                    )
+                )
+            )
+        );
+    }
+});
+
+var Page = React.createClass({
+    render: function() {
+        return (
+            React.createElement('div', {id: 'root', className: 'container-fluid'}, 
+                React.createElement('div', {className: 'row'},
+                    React.createElement('div', {className: 'col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 main'}, 
+                        React.createElement(SectionHeader, {text: 'Questions'}),
+                        React.createElement(QuestionsTable)
+                    )
+                )
+            )
         );
     }
 });
 
 window.onload = function()
 {
-    var Timer = React.createClass({
-        getInitialState: function() {
-            return {secondsElapsed: 0};
-        },
-        tick: function() {
-            this.setState({secondsElapsed: this.state.secondsElapsed + 1});
-        },
-        componentDidMount: function() {
-            this.interval = setInterval(this.tick, 1000);
-        },
-        componentWillMount: function() {
-            clearInterval(this.interval);
-        },
-        render: function() {
-            return (
-                React.createElement("div", null, "Seconds elapsed: " + this.state.secondsElapsed)
-            );
-        }
-    });
-
-    // React.render(React.createElement(Timer, null), document.querySelector('#content'));
-
-    React.render(React.createElement(QuestionsTable, null), document.querySelector('#content'));
-
-    console.log(new WelcomeDisplay);
-    console.log("Page loading complete!");
+    React.render(React.createElement(Page, null), document.querySelector('body'));
 }
